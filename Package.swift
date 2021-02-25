5c71838b9ec0e872883e4214e790dee15bae21ab
Adam Fowler <adamfowler71@gmail.com>
2021-02-25 16:25:12 +0000

    Update to hummingbird v0.5.0

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", from: "0.3.0"),
         .package(url: "https://github.com/swift-extras/swift-extras-base64.git", from: "0.5.0"),
-        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "0.4.0")
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "0.5.0")
     ],
     targets: [
         .target(name: "HummingbirdLambda", dependencies: [
