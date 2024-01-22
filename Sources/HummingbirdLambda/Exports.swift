b0261e3c36ea1960ea324d0babbde89a82b54e79
Adam Fowler <adamfowler71@gmail.com>
2024-01-22 14:15:12 +0000

    Don't include the documentation for Hummingbird

--- Sources/HummingbirdLambda/Exports.swift
+++ Sources/HummingbirdLambda/Exports.swift
@@ -12,4 +12,4 @@
 //
 //===----------------------------------------------------------------------===//
 
-@_exported import Hummingbird
+@_exported @_documentation(visibility: internal) import Hummingbird
