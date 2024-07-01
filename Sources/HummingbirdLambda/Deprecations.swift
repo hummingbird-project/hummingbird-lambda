10c59b878463d671409e16b02f02509b212c5733
Adam Fowler <adamfowler71@gmail.com>
2024-07-01 10:28:47 +0100

    Make deprecations unavailable (#32)

--- Sources/HummingbirdLambda/Deprecations.swift
+++ Sources/HummingbirdLambda/Deprecations.swift
@@ -12,19 +12,17 @@
 //
 //===----------------------------------------------------------------------===//
 
-// Below is a list of deprecated symbols with the "HB" prefix. These are available
+// Below is a list of unavailable symbols with the "HB" prefix. These are available
 // temporarily to ease transition from the old symbols that included the "HB"
 // prefix to the new ones.
-//
-// This file will be removed before we do a 2.0 release
 
-@_documentation(visibility: internal) @available(*, deprecated, renamed: "LambdaFunction")
+@_documentation(visibility: internal) @available(*, unavailable, renamed: "LambdaFunction")
 public typealias HBLambda = LambdaFunction
-@_documentation(visibility: internal) @available(*, deprecated, renamed: "APIGatewayLambdaFunction")
+@_documentation(visibility: internal) @available(*, unavailable, renamed: "APIGatewayLambdaFunction")
 public typealias HBAPIGatewayLambda = APIGatewayLambdaFunction
-@_documentation(visibility: internal) @available(*, deprecated, renamed: "APIGatewayV2LambdaFunction")
+@_documentation(visibility: internal) @available(*, unavailable, renamed: "APIGatewayV2LambdaFunction")
 public typealias HBAPIGatewayV2Lambda = APIGatewayV2LambdaFunction
-@_documentation(visibility: internal) @available(*, deprecated, renamed: "LambdaRequestContext")
+@_documentation(visibility: internal) @available(*, unavailable, renamed: "LambdaRequestContext")
 public typealias HBLambdaRequestContext = LambdaRequestContext
-@_documentation(visibility: internal) @available(*, deprecated, renamed: "BasicLambdaRequestContext")
+@_documentation(visibility: internal) @available(*, unavailable, renamed: "BasicLambdaRequestContext")
 public typealias HBBasicLambdaRequestContext = BasicLambdaRequestContext
