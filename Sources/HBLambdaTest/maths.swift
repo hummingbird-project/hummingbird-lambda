da0b994171f872c5afcb2522859487aa9e88268b
Adam Fowler <adamfowler71@gmail.com>
2025-01-06 17:11:23 +0000

    Use APIGatewayV2

--- Sources/HBLambdaTest/maths.swift
+++ Sources/HBLambdaTest/maths.swift
@@ -18,7 +18,7 @@ import Hummingbird
 import HummingbirdLambda
 import Logging
 
-typealias AppRequestContext = BasicLambdaRequestContext<APIGatewayRequest>
+typealias AppRequestContext = BasicLambdaRequestContext<APIGatewayV2Request>
 
 struct DebugMiddleware: RouterMiddleware {
     typealias Context = AppRequestContext
@@ -34,17 +34,17 @@ struct DebugMiddleware: RouterMiddleware {
     }
 }
 
-struct Operands: Decodable {
-    let lhs: Double
-    let rhs: Double
-}
-
-struct Result: ResponseEncodable {
-    let result: Double
-}
-
 @main
 struct MathsLambda {
+    struct Operands: Decodable {
+        let lhs: Double
+        let rhs: Double
+    }
+
+    struct Result: ResponseEncodable {
+        let result: Double
+    }
+
     static func main() async throws {
         let router = Router(context: AppRequestContext.self)
         router.middlewares.add(DebugMiddleware())
@@ -64,42 +64,10 @@ struct MathsLambda {
             let operands = try await request.decode(as: Operands.self, context: context)
             return Result(result: operands.lhs / operands.rhs)
         }
-        let lambda = APIGatewayLambdaFunction(
+        let lambda = APIGatewayV2LambdaFunction(
             router: router,
             logger: Logger(label: "lambda")
         )
         try await lambda.runService()
     }
 }
-/*
-@main
-struct MathsHandler: APIGatewayLambdaFunction {
-    typealias Context = BasicLambdaRequestContext<APIGatewayRequest>
-
-    init(context: LambdaInitializationContext) {}
-
-    func buildResponder() -> some HTTPResponder<Context> {
-        let router = Router(context: Context.self)
-        router.middlewares.add(DebugMiddleware())
-        router.post("add") { request, context -> Result in
-            let operands = try await request.decode(as: Operands.self, context: context)
-            return Result(result: operands.lhs + operands.rhs)
-        }
-        router.post("subtract") { request, context -> Result in
-            let operands = try await request.decode(as: Operands.self, context: context)
-            return Result(result: operands.lhs - operands.rhs)
-        }
-        router.post("multiply") { request, context -> Result in
-            let operands = try await request.decode(as: Operands.self, context: context)
-            return Result(result: operands.lhs * operands.rhs)
-        }
-        router.post("divide") { request, context -> Result in
-            let operands = try await request.decode(as: Operands.self, context: context)
-            return Result(result: operands.lhs / operands.rhs)
-        }
-        return router.buildResponder()
-    }
-
-    func shutdown() async throws {}
-}
-*/
