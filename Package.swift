4d5c7ec5e0840e8bd7cf0602b8b0688cf4c667df
Adam Fowler <adamfowler71@gmail.com>
2026-02-09 11:03:04 +0000

    Fix deprecated msg, update dependency version requirements (#60)
    * Fix deprecated message
    
    * Update dependency version requirements

--- Package.swift
+++ Package.swift
@@ -13,11 +13,11 @@ let package = Package(
         .executable(name: "HBLambdaTest", targets: ["HBLambdaTest"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/awslabs/swift-aws-lambda-runtime.git", from: "2.0.0"),
-        .package(url: "https://github.com/awslabs/swift-aws-lambda-events.git", from: "1.2.1"),
+        .package(url: "https://github.com/awslabs/swift-aws-lambda-runtime.git", from: "2.4.1"),
+        .package(url: "https://github.com/awslabs/swift-aws-lambda-events.git", from: "1.4.0"),
         .package(url: "https://github.com/swift-extras/swift-extras-base64.git", from: "1.0.0"),
-        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.15.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.81.0"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.18.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.91.0"),
     ],
     targets: [
         .target(
